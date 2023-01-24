3b882aa66db2b0c34ad1d5f4c98cd797fc729682
Mani1358 <mani621998@gmail.com>
2023-01-24 14:17:27 +0530

    Added:UC-4

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,29 @@
 package com.blz.day8;
 
 public class EmployeeWage {
-    public static int isFullTime = 1;
-    private static int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+
+    public static int isPartTime = 1;
+    public static int isFullTime = 2;
+    public static int empPerHour = 20;
+    public static int EMP_RATE_PER_HOUR = 20;
+    public static int empHrs = 0;
+    public static int empWage = 0;
+
+    private static double empCheck = (double) (Math.floor(Math.random() * 10) % 2);
 
     public static void employeeWage() {
-        if (empCheck == isFullTime)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+        if (empCheck == isPartTime) {
+            empHrs = 4;
+        } else if (empCheck == isFullTime) {
+            empHrs = 8;
+        } else {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.printf("Employee Wage is" + empWage);
     }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        System.out.print("Employee Attendance: ");
         employeeWage();
     }
 }
